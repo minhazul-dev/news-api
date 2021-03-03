1f7b751be77252407ff98dac5fc9de22fe70b1b3
minhazulmoon-rgb <69197671+minhazulmoon-rgb@users.noreply.github.com>
2021-03-04 05:33:05 +0600

    second

--- src/components/TopHeadline/TopHeadline.js
+++ src/components/TopHeadline/TopHeadline.js
@@ -6,20 +6,20 @@ import axios from 'axios'
 const TopHeadline = () => {
 
     const [articles, setArticles] = useState([]);
-    // useEffect(()=>{
-    //     const url ='https://newsapi.org/v2/top-headlines?country=us&apiKey=9ab298fd9ed848eabd2edcca718b6421'
-    //     fetch(url)
-    //     .then(response =>response.json())
-    //     .then(data =>setArticles(data.articles))
-        
-    // },[])
-    useEffect(() =>{
+    useEffect(()=>{
         const url ='https://newsapi.org/v2/top-headlines?country=us&apiKey=9ab298fd9ed848eabd2edcca718b6421'
-        axios(url)
-        .then(data => setArticles(data.data.articles))
-
-
+        fetch(url)
+        .then(response =>response.json())
+        .then(data =>setArticles(data.articles))
+        
     },[])
+    // useEffect(() =>{
+    //     const url ='https://newsapi.org/v2/top-headlines?country=us&apiKey=9ab298fd9ed848eabd2edcca718b6421'
+    //     axios(url)
+    //     .then(data => setArticles(data.data.articles))
+
+
+    // },[])
 
 
     return (
